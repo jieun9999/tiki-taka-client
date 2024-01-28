52c493e3b358e896477a460b21fb4ea0f14974ce
정지은 <kindjjee@gmail.com>
2024-01-28 17:09:25 +0900

    프로필 화면_1,4,5 xml 만듦

---
+++ app/src/main/java/com/android/tiki_taka/ui/activity/ProfileActivity3.java
@@ -0,0 +1,39 @@
+package com.android.tiki_taka.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.android.tiki_taka.R;
+
+import java.util.Objects;
+
+public class ProfileActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile3);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("상대방과 연결 끊기");
+        // 뒤로 가기 버튼 활성화
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    // 뒤로 가기 버튼 클릭 이벤트 처리
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
