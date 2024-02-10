c113a7ff5b2cedf9d2be0c1786347f757463b725
정지은 <kindjjee@gmail.com>
2024-02-11 00:51:50 +0900

    ReconnectActivity 리팩토링

--- app/src/main/java/com/android/tiki_taka/ui/activity/Profile/ReconnectActivity.java
+++ app/src/main/java/com/android/tiki_taka/ui/activity/Profile/ReconnectActivity.java
@@ -58,47 +58,11 @@ public class ReconnectActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //1. db의 connect 칼럼 업데이트
                 service.reconnectAccount(userId).enqueue(new Callback<ResponseBody>() {
                     @Override
                     public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
 
-                        // 요청 성공 처리
-                        if (response.isSuccessful()) {
-                            // http 요청 성공시
-                            try {
-                                String responseJson = response.body().string();
-                                JSONObject jsonObject = new JSONObject(responseJson);
-                                boolean success = jsonObject.getBoolean("success");
-                                String message = jsonObject.getString("message");
-
-                                if (success) {
-                                    //db 업데이트 성공 성공
-                                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-
-                                    // 상대방의 connect 칼럼 확인하기
-                                    checkConnectState();
-
-                                } else {
-                                    // 실패
-                                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-                                }
-
-                            } catch (JSONException e) {
-                                // JSON 파싱 오류 처리
-                                e.printStackTrace();
-                                Toast.makeText(getApplicationContext(), "JSON 파싱 오류", Toast.LENGTH_LONG).show();
-                            } catch (IOException e) {
-                                // IOException 처리
-                                e.printStackTrace();
-                                Toast.makeText(getApplicationContext(), "IO 오류", Toast.LENGTH_LONG).show();
-                            }
-
-                        } else {
-                            //서버 응답 오류
-                            Toast.makeText(getApplicationContext(), "서버 응답 오류: " + response.code(), Toast.LENGTH_LONG).show();
-                        }
-
+                        reconnectAccountResponseProcess(response);
                     }
 
                     @Override
@@ -119,6 +83,40 @@ public class ReconnectActivity extends AppCompatActivity {
             }
         });
     }
+    private void reconnectAccountResponseProcess(Response<ResponseBody> response){
+        // 요청 성공 처리
+        if (response.isSuccessful()) {
+            // http 요청 성공시
+            try {
+                String responseJson = response.body().string();
+                JSONObject jsonObject = new JSONObject(responseJson);
+                boolean success = jsonObject.getBoolean("success");
+                String message = jsonObject.getString("message");
+
+                if (success) {
+                    //db 업데이트 성공 성공
+                    showToast(message);
+                    // 상대방의 connect 칼럼 확인하기
+                    checkConnectState();
+
+                } else {
+                    //실패
+                    showToast(message);
+                }
+
+            } catch (JSONException |IOException e) {
+                e.printStackTrace();
+                showToast("오류 생성");
+            }
+
+        } else {
+            showToast("서버 응답 오류");
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -135,63 +133,82 @@ public class ReconnectActivity extends AppCompatActivity {
         service.checkConnectState(userId).enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-
-                try {
-                    if (response.isSuccessful() && response.body() != null) {
-                        String responseBodyString = response.body().string();
-                        JSONObject jsonObject = new JSONObject(responseBodyString);
-
-                        if (jsonObject.getBoolean("success")) {
-                            int partnerState = jsonObject.getInt("partnerState");
-
-                            // 여기에 connectStatus를 기반으로 한 로직을 구현합니다.
-                            if (partnerState == 1) {
-                                //파트너 1
-                                Toast.makeText(getApplicationContext(), "재연결에 성공하셨습니다!",Toast.LENGTH_LONG).show();
-
-                                // 스택 초기화 후 홈화면 으로 이동
-                                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                startActivity(intent);
-                                finish(); // 현재 액티비티 종료
-
-                            } else if (partnerState == 0) {
-                                //파트너 0
-                                Toast.makeText(getApplicationContext(), "상대방은 재연결을 원하지 않습니다.",Toast.LENGTH_LONG).show();
-
-                                // 스택 초기화 후 로그인_1 화면으로 이동
-                                Intent intent = new Intent(getApplicationContext(), SigninActivity1.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                startActivity(intent);
-                                finish(); // 현재 액티비티 종료
-
-                            } else {
-                                // 유저 1, 상대방 1
-                                // 정상 연결
-                                Toast.makeText(getApplicationContext(), "연결에 성공하였습니다.",Toast.LENGTH_LONG).show();
-
-                            }
-                        } else {
-                            // 프로필이 존재하지 않는 경우
-                            String errorMessage = jsonObject.getString("message");
-                            // 에러 메시지 처리
-                            Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_LONG).show();
-                        }
-                    } else {
-                        // 서버 응답 실패 처리
-                        Toast.makeText(getApplicationContext(), "서버 응답 오류: " + response.code(), Toast.LENGTH_LONG).show();
-                    }
-                } catch (IOException | JSONException e) {
-                    // JSON 파싱 오류 처리
-                    e.printStackTrace();
-                }
+                checkConnectStateResponseProcess(response);
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                // 요청 실패 처리
                 Log.e("Network Error", "네트워크 호출 실패: " + t.getMessage());
             }
         });
     }
+
+    private void checkConnectStateResponseProcess( Response<ResponseBody> response){
+        try {
+            if (response.isSuccessful() && response.body() != null) {
+                String responseBodyString = response.body().string();
+                JSONObject jsonObject = new JSONObject(responseBodyString);
+                int partnerState = jsonObject.getInt("partnerState");
+
+                if (jsonObject.getBoolean("success")) {
+                    handleConnectionStatus(partnerState);
+
+                } else {
+                    handleErrorMessage(jsonObject);
+                }
+            } else {
+                showToast("서버 응답 오류");
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleConnectionStatus(int partnerState) throws JSONException {
+        // 여기에 connectStatus를 기반으로 한 로직을 구현합니다.
+
+        if (partnerState == 1) {
+            //파트너 1
+            showToast("재연결에 성공하셨습니다!");
+            navigateToHomeScreen();
+
+        } else if (partnerState == 0) {
+            //파트너 0
+            showToast("상대방은 재연결을 원하지 않습니다.");
+            navigateToLoginScreen();
+
+        } else {
+            // 유저 1, 상대방 1
+            showToast("연결 성공!");
+
+        }
+    }
+
+    private void handleErrorMessage(JSONObject jsonObject) throws JSONException {
+        // 프로필이 존재하지 않는 경우
+        String errorMessage = jsonObject.getString("message");
+        // 에러 메시지 처리
+        showToast(errorMessage);
+    }
+
+    private void navigateToLoginScreen() {
+        // 스택 초기화 후 로그인_1 화면으로 이동
+        Intent intent = new Intent(getApplicationContext(), SigninActivity1.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish(); // 현재 액티비티 종료
+    }
+
+    private void navigateToHomeScreen() {
+        // 스택 초기화 후 홈화면으로 이동
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish(); // 현재 액티비티 종료
+    }
+
+
+
+
+
 }
