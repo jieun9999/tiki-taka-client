10299179b5fc54a3abdc75c57e13428c1267e336
정지은 <kindjjee@gmail.com>
2024-02-10 18:52:58 +0900

    SignupActivity1 리팩토링 (단일 책임 원칙(Single Responsibility Principle, SRP))

--- app/src/main/java/com/android/tiki_taka/ui/activity/Sign/SignupActivity1.java
+++ app/src/main/java/com/android/tiki_taka/ui/activity/Sign/SignupActivity1.java
@@ -168,35 +168,16 @@ public class SignupActivity1 extends AppCompatActivity {
     public void onVerifyButtonClicked() {
         email = emailTextInput.getText().toString();
 
-
         // 이메일 형식이 올바른 경우, 가입된 이메일 확인 요청을 보냄
         Call<Boolean> call = service.checkUserEmail(email);
         call.enqueue(new Callback<Boolean>() {
             @Override
             public void onResponse(Call<Boolean> call, Response<Boolean> response) {
-                if (response.isSuccessful()) {
-                    // 서버에서 응답을 받았을 때
-                    boolean isRegistered = response.body();
-                    if (isRegistered) {
-                        // 이미 가입된 이메일인 경우
-                        AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity1.this);
-                        builder.setMessage("이미 가입된 이메일입니다.")
-                                .setPositiveButton("확인", null)
-                                .show();
-                    } else {
-                        // 가입되지 않은 이메일인 경우
-                        // 이메일 인증 요청 프로세스를 진행합니다.
-                        Log.e("사용가능한 이메일 입니다", "HTTP 코드: " + response.code());
-
-                        // 서버측에서 인증번호 이메일을 보내줌
-                        sendEmail(email);
-                    }
-                }
+                onVerifyButtonClickedProcessResponse(response);
             }
 
             @Override
             public void onFailure(Call<Boolean> call, Throwable t) {
-                //네트워크 오류 처리
                 Log.e("Network Error", "네트워크 호출 실패: " + t.getMessage());
             }
         });
@@ -204,123 +185,137 @@ public class SignupActivity1 extends AppCompatActivity {
 
     }
 
+    private void onVerifyButtonClickedProcessResponse( Response<Boolean> response){
+        if (response.isSuccessful()) {
+            onVerifyButtonHandleSuccessResponse(response);
+        }
+    }
+
+    private void onVerifyButtonHandleSuccessResponse(Response<Boolean> response){
+        // 서버에서 응답을 받았을 때
+        boolean isRegistered = response.body();
+        if (isRegistered) {
+            showAlreadyRegisteredEmailDialog();
+        } else {
+            Log.e("사용가능한 이메일 입니다", "HTTP 코드: " + response.code());
+
+            // 서버측에서 인증번호 이메일을 보내줌
+            sendEmail(email);
+        }
+    }
+
+    private void showAlreadyRegisteredEmailDialog(){
+        //이미 가입된 이메일인 경우를 알리는 경고 다이얼로그를 표시함.
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity1.this);
+        builder.setMessage("이미 가입된 이메일입니다.")
+                .setPositiveButton("확인", null)
+                .show();
+    }
+
 
     public void sendEmail(String email) {
-        // 1. 서버에 사용자가 입력한 이메일 주소 전송
         Call<Boolean> call = service.sendEmail(email);
-
-        //2. 서버의 인증번호전송 성공여부에 따라 토스트 출력
         call.enqueue(new Callback<Boolean>() {
             @Override
             public void onResponse(Call<Boolean> call, Response<Boolean> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    // 서버에서 응답도 성공하고, 응답이 빈 값이 아닐때
-                    Boolean result = response.body();
-                    // 서버에서 인증번호 전송에 성공했을 때의 처리
-                    if (result) {
-                        // 인증번호 전송 성공
-                        Toast.makeText(getApplicationContext(), "인증 메일이 성공적으로 발송되었습니다.", Toast.LENGTH_LONG).show();
-                    } else {
-                        // 인증번호 전송 실패
-                        Toast.makeText(getApplicationContext(), "인증 메일 발송에 실패하였습니다.'", Toast.LENGTH_LONG).show();
-                    }
-                } else {
-                    // 서버에서 인증번호 전송 실패 처리
-                    Toast.makeText(getApplicationContext(), "서버 응답 오류: " + response.code(), Toast.LENGTH_LONG).show();
-                }
+                sendEmailProcessResponse(response);
             }
 
             @Override
             public void onFailure(Call<Boolean> call, Throwable t) {
-                // 네트워크 오류 처리
                 Toast.makeText(getApplicationContext(), "네트워크 오류: " + t.getMessage(), Toast.LENGTH_LONG).show();
             }
 
         });
     }
+
+    private void sendEmailProcessResponse(Response<Boolean> response){
+        if (response.isSuccessful() && response.body() != null) {
+            // 서버에서 응답도 성공하고, 응답이 빈 값이 아닐때
+            Boolean result = response.body();
+
+            // 서버에서 인증번호 전송에 성공했을 때의 처리
+            if (result) {
+                showToast("인증 메일이 성공적으로 발송되었습니다.");
+
+            } else {
+                showToast("인증 메일 발송에 실패하였습니다.");
+            }
+        } else {
+            showToast("서버 응답 오류");
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+
 
     public void onConfirmButtonClicked() {
         String code = codeTextInput.getText().toString();
 
-        //1. 서버에 사용자가 입력한 인증번호 전송
         Call<ResponseBody> call = service.sendAuthCode(email, code);
-
-        //2. 서버 응답을 Boolean 대신 JSON 객체로 받아서 처리
-        //버에서는 json 형식으로 받고 이것을 클라이언트가 처리할 수 있는 jsonObject로 파싱
         call.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if (response.isSuccessful()) {
-                    // 서버에서 응답이 올때
-
-                    try {
-                        String responseJson = response.body().string();
-                        //response.body().string() 메서드를 사용하여 ResponseBody를 문자열로 읽어오는 것
-                        //.toString() 과 다름
-                        JSONObject jsonObject = new JSONObject(responseJson);
-                        boolean success = jsonObject.getBoolean("success");
-                        String message = jsonObject.getString("message");
-                        int userId = jsonObject.getInt("userId");
-
-                        //사용자아이디(user_id)를 db에서 가져와서 기기 내에 저장함
-                        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putInt("userId", userId);
-                        editor.apply();
-
-                        if (success) {
-                            // 인증번호 일치
-                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-                        } else {
-                            // 인증번호 불일치 or 만료됨
-                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-                        }
-                    } catch (JSONException | IOException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                } else {
-                    // 서버에서 인증번호 전송 실패 처리
-                    Toast.makeText(getApplicationContext(), "서버 응답 오류: " + response.code(), Toast.LENGTH_LONG).show();
-                }
+                confirmButtonResponseProcess(response);
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                // 네트워크 오류 처리
                 Toast.makeText(getApplicationContext(), "네트워크 오류: " + t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
-
     }
 
-    public void sendPw(String pw){
-        // 1. 서버에 사용자가 입력한 비번 전송
-        Call<Boolean> call = service.sendPass(email, pw);
+    private void confirmButtonResponseProcess(Response<ResponseBody> response){
+        if (response.isSuccessful()) {
+            // 서버에서 응답이 올때
 
-        //2. 서버의 성공여부에 따라 다른 처리
+            try {
+                String responseJson = response.body().string();
+                JSONObject jsonObject = new JSONObject(responseJson);
+                boolean success = jsonObject.getBoolean("success");
+                String message = jsonObject.getString("message");
+                int userId = jsonObject.getInt("userId");
+
+                saveUserIdToSharedPreferences(userId);
+
+                if (success) {
+                    // 인증번호 일치
+                   showToast(message);
+
+                } else {
+                    // 인증번호 불일치 or 만료됨
+                    showToast(message);
+                }
+            } catch (JSONException | IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        } else {
+            showToast("서버 응답 오류");
+        }
+    }
+
+    private void saveUserIdToSharedPreferences(int userId) {
+        // 사용자 아이디를 SharedPreferences에 저장합니다.
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("userId", userId);
+        editor.apply();
+    }
+
+
+
+
+    public void sendPw(String pw){
+        Call<Boolean> call = service.sendPass(email, pw);
         call.enqueue(new Callback<Boolean>() {
             @Override
             public void onResponse(Call<Boolean> call, Response<Boolean> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    // 서버에서 응답도 성공하고, 응답이 빈 값이 아닐때
-                    Boolean result = response.body();
-                    // 서버에서 인증번호 전송에 성공했을 때의 처리
-                    if (result) {
-                        // 비밀번호 db에 저장 성공
-
-                        // 회원가입_2로 이동
-                        Intent intent = new Intent(SignupActivity1.this, SignupActivity2.class);
-                        startActivity(intent);
-
-                    } else {
-                        // 비밀번호 저장 실패
-                        Toast.makeText(getApplicationContext(), "회원가입에 실패하였습니다.'", Toast.LENGTH_LONG).show();
-                    }
-                } else {
-                    // 서버에서 인증번호 전송 실패 처리
-                    Toast.makeText(getApplicationContext(), "서버 응답 오류: " + response.code(), Toast.LENGTH_LONG).show();
-                }
+                sendPwResponseProcess(response);
             }
 
             @Override
@@ -328,6 +323,28 @@ public class SignupActivity1 extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void sendPwResponseProcess(Response<Boolean> response){
+        if (response.isSuccessful() && response.body() != null) {
+            Boolean result = response.body();
+            if (result) {
+                handleSuccessResponse();
+
+            } else {
+                // 비밀번호 저장 실패
+                showToast("회원가입을 위한 비밀번호 저장에 실패하였습니다.");
+
+            }
+        } else {
+            showToast("서버 응답 오류");
+        }
+    }
+
+    private void handleSuccessResponse(){
+        // 회원가입_2로 이동
+        Intent intent = new Intent(SignupActivity1.this, SignupActivity2.class);
+        startActivity(intent);
     }
 
 }
