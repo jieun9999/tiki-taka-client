388a1c4b87f5c7f823963ace6126827c005dacc1
정지은 <kindjjee@gmail.com>
2024-02-11 13:59:41 +0900

    StoryFolderActivity1 리팩토링

--- app/src/main/java/com/android/tiki_taka/ui/activity/Album/StoryFolderActivity1.java
+++ app/src/main/java/com/android/tiki_taka/ui/activity/Album/StoryFolderActivity1.java
@@ -25,6 +25,7 @@ import com.android.tiki_taka.models.responses.StoryFoldersResponse;
 import com.android.tiki_taka.services.StoryApiService;
 import com.android.tiki_taka.utils.DateUtils;
 import com.android.tiki_taka.utils.RetrofitClient;
+import com.android.tiki_taka.utils.SharedPreferencesHelper;
 import com.bumptech.glide.Glide;
 
 import java.text.ParseException;
@@ -38,9 +39,9 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class StoryFolderActivity1 extends AppCompatActivity {
-    int folderId; //폴더 식별 정보
+    int folderId;
     StoryApiService service;
-    int userId; // 유저 식별 정보
+    int userId;
     StoryCardAdapter adapter;
     RecyclerView recyclerView;
 
@@ -64,8 +65,7 @@ public class StoryFolderActivity1 extends AppCompatActivity {
         Retrofit retrofit = RetrofitClient.getClient();
         // Retrofit을 통해 ApiService 인터페이스를 구현한 서비스 인스턴스를 생성
         service = retrofit.create(StoryApiService.class);
-        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
-        userId = sharedPreferences.getInt("userId", -1); // 기본값으로 -1이나 다른 유효하지 않은 값을 설정
+        userId = SharedPreferencesHelper.getUserId(this);
 
         // 리사이클러뷰 초기화
         recyclerView = findViewById(R.id.recyclerView);
@@ -80,8 +80,6 @@ public class StoryFolderActivity1 extends AppCompatActivity {
 
         // 가져온 아이템 ID를 사용하여 세부 정보를 표시하거나 데이터를 로드
         if (folderId != -1) {
-            // 유효한 아이템 ID인 경우, 해당 ID를 사용하여 데이터를 로드하거나 처리
-            // 예: 데이터베이스 조회, API 요청 등
 
             //1. 대표사진 가져오기 (storyFolder 테이블에서)
             loadThumbNail();
@@ -100,44 +98,8 @@ public class StoryFolderActivity1 extends AppCompatActivity {
         service.getThumbNail(folderId).enqueue(new Callback<StoryFolderResponse>() {
             @Override
             public void onResponse(Call<StoryFolderResponse> call, Response<StoryFolderResponse> response) {
-                if(response.isSuccessful() && response.body() != null){
-                    // 요청 성공 + 응답 존재
-
-                    StoryFolderResponse storyFolderResponse = response.body();
-                    if(storyFolderResponse.isSuccess()){
-                        //success가 true인 경우,
-                        StoryFolderDto storyFolderDto = storyFolderResponse.getStoryFolder();
-
-                        //썸네일 뷰 할당하기
-                        TextView thumbDateView = findViewById(R.id.textView26);
-                        TextView thumbTitleView = findViewById(R.id.textView27);
-                        TextView thumbLocView = findViewById(R.id.textView28);
-                        ImageView thumbBackImgView = findViewById(R.id.imageView26);
-
-                        // 서버 날짜 문자열(2024-01-31 12:24:40) => 2023년 12월 25일 (월) 변환
-                        String inputDateString = storyFolderDto.getUpdatedAt();
-                        try {
-                            String outputDateString = DateUtils.convertDateString(inputDateString);
-                            thumbDateView.setText(outputDateString);
-                        } catch (ParseException e) {
-                            throw new RuntimeException(e);
-                        }
-                        thumbTitleView.setText(storyFolderDto.getTitle());
-                        thumbLocView.setText(storyFolderDto.getLocation());
-                        // 이미지는 글라이드로 할당
-                        Glide.with(getApplicationContext())
-                                .load(storyFolderDto.getDisplayImageUrl())
-                                .into(thumbBackImgView);
-
-                        String message = storyFolderResponse.getMessage();
-                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-
-                    }else {
-                        //success가 false인 경우,
-                        String message = storyFolderResponse.getMessage();
-                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-                    }
-
+                if(response.isSuccessful() && response.body() != null) {
+                    processThumbNailResponse(response);
                 }else {
                     // 응답 실패
                     Log.e("Error", "서버에서 불러오기에 실패: " + response.code());
@@ -152,29 +114,60 @@ public class StoryFolderActivity1 extends AppCompatActivity {
         });
     }
 
+    private void processThumbNailResponse(Response<StoryFolderResponse> response){
+        // 요청 성공 + 응답 존재
+
+        StoryFolderResponse storyFolderResponse = response.body();
+        if(storyFolderResponse.isSuccess()){
+            updateUIOnSuccess(storyFolderResponse);
+
+        }else {
+            handleFailure(storyFolderResponse);
+
+        }
+    }
+
+    private void updateUIOnSuccess(StoryFolderResponse storyFolderResponse){
+        //success가 true인 경우,
+        StoryFolderDto storyFolderDto = storyFolderResponse.getStoryFolder();
+
+        //썸네일 뷰 할당하기
+        TextView thumbDateView = findViewById(R.id.textView26);
+        TextView thumbTitleView = findViewById(R.id.textView27);
+        TextView thumbLocView = findViewById(R.id.textView28);
+        ImageView thumbBackImgView = findViewById(R.id.imageView26);
+
+        // 서버 날짜 문자열(2024-01-31 12:24:40) => 2023년 12월 25일 (월) 변환
+        String inputDateString = storyFolderDto.getUpdatedAt();
+        try {
+            String outputDateString = DateUtils.convertDateString(inputDateString);
+            thumbDateView.setText(outputDateString);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        thumbTitleView.setText(storyFolderDto.getTitle());
+        thumbLocView.setText(storyFolderDto.getLocation());
+        // 이미지는 글라이드로 할당
+        Glide.with(getApplicationContext())
+                .load(storyFolderDto.getDisplayImageUrl())
+                .into(thumbBackImgView);
+
+        String message = storyFolderResponse.getMessage();
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    private void handleFailure(StoryFolderResponse storyFolderResponse){
+        //success가 false인 경우,
+        String message = storyFolderResponse.getMessage();
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
     private void loadStoryCards(){
         service.getStoryCards(folderId).enqueue(new Callback<StoryCardsResponse>() {
             @Override
             public void onResponse(Call<StoryCardsResponse> call, Response<StoryCardsResponse> response) {
-                if(response.isSuccessful() && response.body() != null){
-                    // 요청 성공 + 응답 존재
-
-                    StoryCardsResponse storyCardsResponse = response.body();
-                    if(storyCardsResponse.isSuccess()){
-                        //success가 true인 경우,
-                        List<StoryCardDto> storyCardDtos = storyCardsResponse.getStoryCards();
-
-                        // 서버에서 가져온 리스트를 어댑터에 추가함
-                        adapter.setData(storyCardDtos);
-
-                        String message = storyCardsResponse.getMessage();
-                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-
-                    }else {
-                        //success가 false인 경우,
-                        String message = storyCardsResponse.getMessage();
-                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-                    }
+                if(response.isSuccessful() && response.body() != null){   // 요청 성공 + 응답 존재
+                    handleStoryCardsResponse(response);
 
                 }else {
                     // 응답 실패
@@ -188,5 +181,35 @@ public class StoryFolderActivity1 extends AppCompatActivity {
                 Log.e("Network Error", "네트워크 호출 실패: " + t.getMessage());
             }
         });
+    }
+
+    private void handleStoryCardsResponse(Response<StoryCardsResponse> response){
+
+        StoryCardsResponse storyCardsResponse = response.body();
+        if(storyCardsResponse.isSuccess()){
+            updateUIonSuccess(storyCardsResponse);
+
+        }else {
+            handleFailure(storyCardsResponse);
+        }
+
+    }
+
+    //"오버로딩(Overloading)"이라고 합니다. 함수 오버로딩은 같은 이름의 함수가 서로 다른 매개변수를 가질 수 있도록 허용
+    private void updateUIonSuccess(StoryCardsResponse storyCardsResponse){
+        //success가 true인 경우,
+        List<StoryCardDto> storyCardDtos = storyCardsResponse.getStoryCards();
+
+        // 서버에서 가져온 리스트를 어댑터에 추가함
+        adapter.setData(storyCardDtos);
+
+        String message = storyCardsResponse.getMessage();
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void handleFailure(StoryCardsResponse storyCardsResponse){
+        //success가 false인 경우,
+        String message = storyCardsResponse.getMessage();
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
