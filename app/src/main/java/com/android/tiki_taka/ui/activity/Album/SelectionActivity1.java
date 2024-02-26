ffdd74af1abd4e8d5aef81bbf9ee411f03312f0c
정지은 <kindjjee@gmail.com>
2024-02-26 22:47:37 +0900

    카메라 사진 업로드 하기

--- app/src/main/java/com/android/tiki_taka/ui/activity/Album/SelectionActivity1.java
+++ app/src/main/java/com/android/tiki_taka/ui/activity/Album/SelectionActivity1.java
@@ -2,6 +2,7 @@ package com.android.tiki_taka.ui.activity.Album;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.annotation.SuppressLint;
 import android.content.ClipData;
@@ -13,12 +14,17 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.tiki_taka.R;
+import com.android.tiki_taka.utils.ImageUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class SelectionActivity1 extends AppCompatActivity {
 
     private static final int PHOTO_PICKER_MULTI_SELECT_REQUEST_CODE = 1 ;
+    private static final int REQUEST_IMAGE_CAPTURE = 2;
+    private Uri imageUri; //카메라 앱이 전달받을 파일경로
     ArrayList<Uri> selectedUris = new ArrayList<>();
 
     @Override
@@ -26,17 +32,41 @@ public class SelectionActivity1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection1);
 
-        ImageView xBtn = findViewById(R.id.imageView40);
-        xBtn.setOnClickListener(v -> dismissActivity());
+        //카메라
+        ImageView cameraIcon = findViewById(R.id.imageView30);
+        cameraIcon.setOnClickListener( v -> openCamera());
 
+        //사진
         ImageView photoIcon = findViewById(R.id.imageView35);
         photoIcon.setOnClickListener(v -> launchPhotoPickerInMultiSelectedMode());
 
+        ImageView xBtn = findViewById(R.id.imageView40);
+        xBtn.setOnClickListener(v -> dismissActivity());
+
+
     }
 
-    private void dismissActivity(){
-        finish();
+    // 카메라 인텐트를 생성하고, EXTRA_OUTPUT에 사진을 저장할 파일의 Uri를 전달
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = ImageUtils.createImageFile(this); //파일 경로를 만듦
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            if (photoFile != null) {
+                imageUri = FileProvider.getUriForFile(this,
+                        "com.android.tiki_taka.fileprovider", //authorities 문자열이 AndroidManifest.xml 파일과 정확히 일치하는지 확인!
+                        photoFile);
+                // 로컬 파일 시스템에 있는 파일(photoFile)에 대한 Content Uri를 생성
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
     }
+
 
     @SuppressLint("IntentReset")
     private void launchPhotoPickerInMultiSelectedMode(){
@@ -47,11 +77,22 @@ public class SelectionActivity1 extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(intent, "Select Pictures"), PHOTO_PICKER_MULTI_SELECT_REQUEST_CODE);
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
+    private void dismissActivity(){
+        finish();
+    }
 
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK && data != null) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            selectedUris.clear();
+            selectedUris.add(imageUri);
+            Intent intent = new Intent(SelectionActivity1.this, StoryWritingActivity1.class);
+            intent.putParcelableArrayListExtra("selectedUris", selectedUris);
+            startActivity(intent);
+
+
+        } else if (requestCode == PHOTO_PICKER_MULTI_SELECT_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
             // 초기화를 여기서 수행하면 사용자가 다시 이미지 선택을 시작할 때마다 리스트가 초기화됩니다.
             selectedUris.clear();
 
@@ -72,16 +113,11 @@ public class SelectionActivity1 extends AppCompatActivity {
                 // 단일 사진 선택
                 selectedUris.add(data.getData());
             }
-
             if(!selectedUris.isEmpty()){
                 Intent intent = new Intent(SelectionActivity1.this, StoryWritingActivity1.class);
                 intent.putParcelableArrayListExtra("selectedUris", selectedUris);
                 startActivity(intent);
             }
-
-        } else {
-            //이미지 미선택
-            showToast("이미지를 선택하지 않았습니다.");
         }
     }
 
