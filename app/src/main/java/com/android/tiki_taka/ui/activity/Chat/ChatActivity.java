913c0d6c806cbfc58697f9ce563a91881bb90065
정지은 <kindjjee@gmail.com>
2024-03-18 16:52:41 +0900

    ChatActivity 커스텀 툴바 세팅

--- app/src/main/java/com/android/tiki_taka/ui/activity/Chat/ChatActivity.java
+++ app/src/main/java/com/android/tiki_taka/ui/activity/Chat/ChatActivity.java
@@ -3,8 +3,12 @@ package com.android.tiki_taka.ui.activity.Chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.tiki_taka.R;
+import com.android.tiki_taka.models.request.StoryCardRequest;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -12,5 +16,12 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        setupUIListeners();
+    }
+
+    private void setupUIListeners(){
+        ImageView cancelBtn = findViewById(R.id.imageView36);
+        cancelBtn.setOnClickListener( v -> finish());
     }
 }
